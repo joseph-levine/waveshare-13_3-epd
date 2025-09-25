913ca8f0158101f1e620d231e3efb1df3853785c
Joseph Levine <adauctus@gmail.com>
2025-09-24 19:25:33 -0500

    colors

--- src/e_paper_display/color.rs
+++ src/e_paper_display/color.rs
@@ -1,4 +1,7 @@
-use crate::image::color::RGB;
+use image::imageops::ColorMap;
+use image::Rgb;
+use palette::{rgb, Lab};
+use palette::color_difference::HyAb;
 
 #[derive(Debug, Copy, Clone)]
 #[repr(u8)]
@@ -11,15 +14,35 @@ pub enum DisplayColor {
     Green = 0x05,
 }
 
-impl From<DisplayColor> for RGB<u8> {
-    fn from(value: DisplayColor) -> Self {
-        match value {
-            DisplayColor::Black => RGB { red: 0, green: 0, blue: 0 },
-            DisplayColor::White => RGB { red: 255, green: 255, blue: 255 },
-            DisplayColor::Yellow => RGB { red: 255, green: 243, blue: 56 },
-            DisplayColor::Red => RGB { red: 191, green: 0, blue: 0 },
-            DisplayColor::Blue => RGB { red: 100, green: 64, blue: 255 },
-            DisplayColor::Green => RGB { red: 67, green: 138, blue: 28 },
+pub struct EPaperMap {
+    colormap: Vec<Lab>,
+}
+
+impl EPaperMap {
+    pub fn new() -> Self {
+        Self {
+            colormap: vec![
+                Lab::from(rgb::Rgb::from([0, 0, 0])),
+                Lab::from(rgb::Rgb::from([255, 255, 255])),
+                Lab::from(rgb::Rgb::from([255, 243, 56])),
+                Lab::from(rgb::Rgb::from([191, 0, 0])),
+                Lab::from(rgb::Rgb::from([100, 64, 255])),
+                Lab::from(rgb::Rgb::from([67, 138, 28])),
+            ]
         }
+    }
+}
+
+impl ColorMap for EPaperMap {
+    type Color = Rgb<u8>;
+
+    fn index_of(&self, color: &Self::Color) -> usize {
+        let lab_color = Lab::from(rgb::Rgb::from(color.0));
+        let distances = self.colormap.map(|l| l.hybrid_distance(lab_color));
+        distances.position(distances.min())
+    }
+
+    fn map_color(&self, color: &mut Self::Color) {
+        todo!()
     }
 }
