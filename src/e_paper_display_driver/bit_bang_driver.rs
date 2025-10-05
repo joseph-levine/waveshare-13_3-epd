76dc5903509be56a619d54be9938e581e4c6322a
Joseph Levine <adauctus@gmail.com>
2025-10-04 22:04:17 -0500

    getting closer?

--- src/e_paper_display_driver/bit_bang_driver.rs
+++ src/e_paper_display_driver/bit_bang_driver.rs
@@ -1,16 +1,16 @@
+// use crate::e_paper_display_driver::bcm_peripherals::Level::{High,Low};
 use crate::display_constants::{DISPLAY_BYTES_PER_CHIP, HALF_WIDTH, HEIGHT, WIDTH};
 // use crate::e_paper_display_driver::gpio_pin::Level::{High, Low};
 use crate::e_paper_display_driver::{command_code::CommandCode, gpio_pin::GpioPin};
+// use linux_embedded_hal::nb::block;
+use rppal::gpio::Level::{High, Low};
+use rppal::gpio::{Error as GpioError, Gpio, InputPin, OutputPin};
 use std::cmp::PartialEq;
 use std::io::Error as IoError;
-use std::thread;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-// use linux_embedded_hal::nb::block;
-use rppal::gpio::{Gpio, InputPin, OutputPin, Error as GpioError};
-use rppal::gpio::Level::{High, Low};
 use thiserror::Error;
-use tracing::info;
+use tracing::{debug, info};
 
 #[derive(Debug, Error)]
 pub enum EpdError {
@@ -30,7 +30,6 @@ enum SelectedChip {
 
 #[derive(Debug)]
 pub struct EPaperDisplayBBDriver {
-    gpio: Gpio,
     data_pin: OutputPin,
     chip_select_main_pin: OutputPin,
     chip_select_peri_pin: OutputPin,
@@ -42,13 +41,11 @@ pub struct EPaperDisplayBBDriver {
     selected_chip: SelectedChip,
 }
 
-fn spin_sleep(duration: Duration) -> u64 {
-    let mut loops = 0u64;
-    let start = Instant::now();
-    while Instant::now().duration_since(start) < duration {
-        loops += 1;
+fn spin_sleep() {
+    let mut loops = 10;
+    while loops > 0 {
+        loops -= 1;
     }
-    loops
 }
 
 impl EPaperDisplayBBDriver {
@@ -64,16 +61,22 @@ impl EPaperDisplayBBDriver {
         let busy_pin = gpio.get(GpioPin::BusyPin as u8)?.into_input();
         let mut power_pin = gpio.get(GpioPin::PowerPin as u8)?.into_output();
 
+        debug!("{:?} low", GpioPin::SerialClockPin as u8);
         clock_pin.set_low();
+        debug!("{:?} low", GpioPin::SerialDataPin as u8);
         data_pin.set_low();
+        debug!("{:?} low", GpioPin::SerialSelectMainPin as u8);
         chip_select_main_pin.set_low();
+        debug!("{:?} low", GpioPin::SerialSelectPeriPin as u8);
         chip_select_peri_pin.set_low();
+        debug!("{:?} low", GpioPin::DataCommandPin as u8);
         data_or_cmd_pin.set_low();
+        debug!("{:?} low", GpioPin::BusyPin as u8);
         reset_pin.set_low();
+        debug!("{:?} high", GpioPin::PowerPin as u8);
         power_pin.set_high();
 
         let mut this = EPaperDisplayBBDriver {
-            gpio,
             data_pin,
             clock_pin,
             data_or_cmd_pin,
@@ -114,30 +117,43 @@ impl EPaperDisplayBBDriver {
     }
 
     fn spi_write(&mut self, bytes: &[u8]) {
+        if bytes.len() > 32 {
+            debug!("Spi write to {} bytes", bytes.len());
+        } else {
+            debug!("Spi write {:02X?}", bytes);
+        }
         for byte in bytes {
             let mut b = byte.clone();
-            for _i in 0..u8::MAX {
-                let _ = spin_sleep(Duration::from_micros(1));
+            for _i in 0..8 {
+                spin_sleep();
                 self.clock_pin.write(Low);
                 self.data_pin
-                    .write(if b & 0x80 == 0x80 { High } else { Low } );
+                    .write(if b & 0x80 == 0x80 { High } else { Low });
                 b = b << 1;
-                let _ = spin_sleep(Duration::from_micros(1));
+                spin_sleep();
                 self.clock_pin.write(High);
             }
         }
-        info!("spi written");
     }
 
-    fn select_chip(&mut self, new_selection: SelectedChip){
+    fn select_chip(&mut self, new_selection: SelectedChip) {
         if new_selection == self.selected_chip {
-            return ;
+            return;
         }
-        // low is selected
-        // should set main on?
-        // if new in (both, main) and selected not in (both, main)
-        self.chip_select_main_pin.write(if new_selection == SelectedChip::Main || new_selection == SelectedChip::Both { Low } else { High });
-        self.chip_select_peri_pin.write(if new_selection == SelectedChip::Main || new_selection == SelectedChip::Peri { Low } else { High });
+        let main_selected = [SelectedChip::Main, SelectedChip::Both].contains(&self.selected_chip);
+        let select_main = [SelectedChip::Main, SelectedChip::Both].contains(&new_selection);
+        let peri_selected = [SelectedChip::Peri, SelectedChip::Both].contains(&self.selected_chip);
+        let select_peri = [SelectedChip::Peri, SelectedChip::Both].contains(&new_selection);
+        if main_selected != select_main {
+            debug!("digital_write pin: 8, value: {}", if select_main {"0"} else {"1"} );
+            self.chip_select_main_pin
+                .write(if select_main { Low } else { High });
+        }
+        if peri_selected != select_peri {
+            debug!("digital_write pin: 7, value: {}", if select_peri {"0"} else {"1"} );
+            self.chip_select_peri_pin
+                .write(if select_peri { Low } else { High });
+        }
         self.selected_chip = new_selection;
     }
 
@@ -145,7 +161,7 @@ impl EPaperDisplayBBDriver {
         &mut self,
         command_code: CommandCode,
         selected_chip: SelectedChip,
-    )  {
+    ) {
         self.select_chip(selected_chip);
         let mut full_cmd = vec![command_code.cmd()];
         if let Some(data) = command_code.data() {
@@ -156,6 +172,7 @@ impl EPaperDisplayBBDriver {
     }
 
     fn wait_for_not_busy(&self) {
+        sleep(Duration::from_millis(20));
         info!("waiting for not busy");
         while self.busy_pin.read() == Low {
             sleep(Duration::from_millis(10));
@@ -187,6 +204,7 @@ impl EPaperDisplayBBDriver {
 
     fn reset(&mut self) {
         for l in [High, Low, High, Low, High] {
+            debug!("Reset: {:?}", &l);
             self.reset_pin.write(l);
             sleep(Duration::from_millis(30));
         }
@@ -197,15 +215,17 @@ impl EPaperDisplayBBDriver {
     pub fn clear_screen(&mut self) {
         let zeros: &[u8; DISPLAY_BYTES_PER_CHIP] = &[0u8; DISPLAY_BYTES_PER_CHIP];
         self.select_chip(SelectedChip::Main);
+        self.spi_write(&[CommandCode::Dtm.cmd()]);
         self.spi_write(zeros);
         self.select_chip(SelectedChip::Neither);
         self.select_chip(SelectedChip::Peri);
+        self.spi_write(&[CommandCode::Dtm.cmd()]);
         self.spi_write(zeros);
         self.select_chip(SelectedChip::Neither);
         self.turn_display_on();
     }
 
-    pub fn send_image(&mut self, image: &[u8])  {
+    pub fn send_image(&mut self, image: &[u8]) {
         assert_eq!(image.len(), HEIGHT * WIDTH / 2);
         let mut top: [u8; DISPLAY_BYTES_PER_CHIP] = [0u8; DISPLAY_BYTES_PER_CHIP];
         let mut bottom: [u8; DISPLAY_BYTES_PER_CHIP] = [0u8; DISPLAY_BYTES_PER_CHIP];
