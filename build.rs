cfebc72d808dae6441714213bd1d03f0a4013451
Joseph Levine <adauctus@gmail.com>
2025-10-06 16:53:09 -0500

    imports

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::path::PathBuf;
+use bindgen::{Builder, CargoCallbacks};
+
 fn main() {
     println!("cargo:rustc-link-search=/usr/local/lib");
     println!("cargo:rustc-link-lib=libbcm2835");
@@ -5,13 +9,13 @@ fn main() {
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
-    let bindings = bindgen::Builder::default()
+    let bindings = Builder::default()
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .parse_callbacks(Box::new(CargoCallbacks::new()))
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
