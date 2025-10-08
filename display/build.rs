3606707eead38a87b837da37c7b4f860fcce09cb
Joseph Levine <adauctus@gmail.com>
2025-10-07 20:51:15 -0500

    build.rs restore

--- display/build.rs
+++ display/build.rs
@@ -3,36 +3,35 @@ use std::path::PathBuf;
 
 fn main() {
     // This is the directory where the `c` library is located.
-    let libdir_path = PathBuf::from("hello")
+    let libdir_path = PathBuf::from("bcm2835")
         // Canonicalize the path as `rustc-link-search` requires an absolute
         // path.
         .canonicalize()
         .expect("cannot canonicalize path");
 
     // This is the path to the `c` headers file.
-    let headers_path = libdir_path.join("hello.h");
+    let headers_path = libdir_path.join("bcm2835.h");
     let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
 
     // This is the path to the intermediate object file for our library.
-    let obj_path = libdir_path.join("hello.o");
+    let obj_path = libdir_path.join("bcm2835.o");
     // This is the path to the static library file.
-    let lib_path = libdir_path.join("libhello.a");
+    let lib_path = libdir_path.join("libbcm2835.a");
 
     // Tell cargo to look for shared libraries in the specified directory
     println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
 
-    // Tell cargo to tell rustc to link our `hello` library. Cargo will
-    // automatically know it must look for a `libhello.a` file.
-    println!("cargo:rustc-link-lib=hello");
+    // Tell cargo to tell rustc to link our `bcm2835` library. Cargo will
+    // automatically know it must look for a `libbcm2835.a` file.
+    println!("cargo:rustc-link-lib=bcm2835");
 
-    // Run `clang` to compile the `hello.c` file into a `hello.o` object file.
+    // Run `clang` to compile the `bcm2835.c` file into a `bcm2835.o` object file.
     // Unwrap if it is not possible to spawn the process.
     if !std::process::Command::new("clang")
-        .arg("--target=aarch64-unknown-linux-gnu")
         .arg("-c")
         .arg("-o")
         .arg(&obj_path)
-        .arg(libdir_path.join("hello.c"))
+        .arg(libdir_path.join("bcm2835.c"))
         .output()
         .expect("could not spawn `clang`")
         .status
@@ -42,7 +41,7 @@ fn main() {
         panic!("could not compile object file");
     }
 
-    // Run `ar` to generate the `libhello.a` file from the `hello.o` file.
+    // Run `ar` to generate the `libbcm2835.a` file from the `bcm2835.o` file.
     // Unwrap if it is not possible to spawn the process.
     if !std::process::Command::new("ar")
         .arg("rcs")
