7f1411f31c7ecb0e83e68c0953ebbebc42bd501c
Joseph Levine <adauctus@gmail.com>
2025-10-12 13:04:10 -0500

    bin -> lib

--- convert/src/lib.rs
+++ convert/src/lib.rs
@@ -3,31 +3,24 @@ extern crate core;
 mod color;
 mod display_constants;
 
-use crate::color::{rgb_to_display_4bit, e_paper_color_map::EPaperColorMap};
-use clap::Parser;
+use crate::color::{e_paper_color_map::EPaperColorMap, rgb_to_display_4bit};
+use crate::display_constants::{PIXEL_HEIGHT, PIXEL_WIDTH};
 use image::imageops::{dither, FilterType};
-use image::EncodableLayout;
+use image::{EncodableLayout, ImageError};
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 use tracing::info;
 use tracing::level_filters::LevelFilter;
-use crate::display_constants::{PIXEL_HEIGHT, PIXEL_WIDTH};
 
-#[derive(Debug, Parser)]
-struct Args {
+pub fn convert(
     file: PathBuf,
     out_file: PathBuf,
     dithered_file: Option<PathBuf>,
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    tracing_subscriber::fmt().with_max_level(LevelFilter::DEBUG).init();
-    let args = Args::parse();
-
-    let img = image::open(&args.file)?;
-    info!("Opened image {}", &args.file.display());
+) -> Result<(), ImageError> {
+    let img = image::open(&file)?;
+    info!("Opened image {}", &file.display());
     let img = img.rotate270();
     info!("Rotated");
     let img = img.resize_to_fill(PIXEL_WIDTH, PIXEL_HEIGHT, FilterType::Lanczos3);
@@ -38,7 +31,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let epd_map = EPaperColorMap::new();
     dither(&mut img, &epd_map);
     info!("Dithered");
-    if let Some(dither_path) = args.dithered_file {
+    if let Some(dither_path) = dithered_file {
         img.save(dither_path)?;
         info!("Saved dithered image");
     }
@@ -46,7 +39,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let epd_image = rgb_to_display_4bit(&img);
     info!("Image packed to 4bit format");
 
-    let mut file = File::create(&args.out_file)?;
+    let mut file = File::create(&out_file)?;
     file.write_all(epd_image.as_bytes())?;
     info!("Image written. Done");
     Ok(())
