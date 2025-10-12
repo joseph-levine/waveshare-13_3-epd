e5b4d4e48a8e7baea64cf20facae3e4c255edd11
Joseph Levine <adauctus@gmail.com>
2025-10-12 16:00:34 -0500

    clear on no file, add crontab schedule

--- display/src/main.rs
+++ display/src/main.rs
@@ -13,30 +13,33 @@ use tracing::metadata::LevelFilter;
 
 #[derive(Debug, Parser)]
 struct Args {
-    file: PathBuf,
+    file: Option<PathBuf>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    tracing_subscriber::fmt().with_max_level(LevelFilter::DEBUG).init();
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::DEBUG)
+        .init();
     let args = Args::parse();
     info!("Reading file...");
-    let epd_image = fs::read(&args.file)?;
-
-    let sleep_secs = 20;
-
     info!("File loaded. Init driver.");
     let mut device = Driver {};
     device.init();
     info!("Device init. Clearing display");
     device.clear();
-    info!("Cleared. Sending image...");
-    device.display(&epd_image);
-    info!("Image sent. Sleeping display...");
-    device.sleep();
-    info!("Display asleep. Waiting {}s", sleep_secs);
-    sleep(Duration::from_secs(sleep_secs));
-    info!("Clearing screen");
-    device.sleep();
+    if let Some(file) = args.file {
+        let epd_image = fs::read(file)?;
+
+        let sleep_secs = 20;
+
+        device.clear();
+        info!("Cleared. Sending image...");
+        device.display(&epd_image);
+        info!("Image sent. Sleeping display...");
+        device.sleep();
+        info!("Display asleep. Waiting {}s", sleep_secs);
+        sleep(Duration::from_secs(sleep_secs));
+    }
     info!("Screen clear. Waiting 2s...");
     sleep(Duration::from_secs(2));
     info!("Dropping device...");
